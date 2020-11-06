1932045b6a71bad9dc7f055c462778c226af3079
yavuzsahin <yavuz.sahins@gmail.com>
2020-11-06 22:09:23 +0300

    Database karakter güncelleme ve otomatik veri tabanı tabloları oluşturma özelliği eklendi. Tabloların değerleri ve default değerler eklendi. Veritabanı ayarları adımı eklendi. CSS ile ekran tasarımı yapıldı.

--- setup/setup.php
+++ setup/setup.php
@@ -26,7 +26,7 @@ if(!isset($_GET['step'])){header('location:?step=1');}
     ================================================== -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
 </head>
 <body>
 <div class="container-fluid">
