aec8788ebe2ea76c34f01e51ea3ff2a5bae753ed
yavuzsahin <yavuz.sahins@gmail.com>
2020-11-07 16:04:06 +0300

    Database için configdate.json dosyası oluşturuldu.

--- config.php
+++ config.php
@@ -4,9 +4,9 @@ $inp            = file_get_contents('../configdate.json');
 $tempArray      = json_decode($inp, true);
 print_r($tempArray);
 
-$dbname = "";
-$dbuser = "";
-$dbpass = "";
+$dbname = "fuesacek_db";
+$dbuser = "fuesacek_user";
+$dbpass = "yVz3191:;";
 define('db_name', $dbname);
 define('db_user', $dbuser);
 define('db_pass', $dbpass);
